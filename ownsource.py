7ba3cee47849f84b1e7079c186dac068e60c0677
JakobSchauser <jakob.schauser@gmail.com>
2022-04-18 10:39:04 +0200

    made most of ground work

--- ownsource.py
+++ ownsource.py
@@ -1,2 +1,12 @@
-with open(__file__) as f: 
-    print('\n'.join(f.read().split('\n')))
+import requests
+
+
+url = "https://libraryofbabel.info/search.cgi"
+r = requests.get(url)
+print(r.text)
+
+
+# print(r.Find("a", class_ = "intext"))
+
+# with open(__file__) as f: 
+#     print('\n'.join(f.read().split('\n')))
